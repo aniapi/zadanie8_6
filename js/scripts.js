ee4fa3a1d39e24cefcd02881b7c8c6a32657aeaa
Anna Piwnik <a.piwnik@o2.pl>
2017-12-28 18:42:18 +0100

    Delete script.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,17 @@ var a = 10,
 	value = (a * a) - (2 * a * b) + (b * b);
 
 alert(value);
-prompt(value);  
 
 console.log(value);
+
+if (value > 0) {
+	console.log('wynik dodatni');
+} else if (value < 0) {
+	console.log('wynik ujemny');
+}
+
+if (value == 0) {
+	console.log('yes');
+} else if (value != 0) {
+	console.log('no');
+}
